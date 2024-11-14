5009a259c7f913759a629c4ebfbcaa8d5e006afe
mhsaputra <m.h.saputra@utwente.nl>
2024-11-14 10:42:48 +0100

    Plotting rain and floods in soil MyScript.py

---
+++ MyScript.py
@@ -0,0 +1,7 @@
+import something
+plot rain soil
+
+# maybe try floods in the model
+plot flood soil
+
+import "C:\Users\mhadi\Desktop\swc-python\python-novice-inflammation-data\data\inflammation-01.csv"
